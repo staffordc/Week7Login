9869be5d4eeced496acb5a76dd9e42d8fa1c514f
staffordc <stafford.carh@gmail.com>
2018-08-31 12:02:54 -0400

    VS added

---
+++ Week7Login/Week7Login/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Week7Login.MvcApplication" Language="C#" %>
---
+++ Week7Login/Week7Login/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Week7Login.Startup))]
+namespace Week7Login
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
